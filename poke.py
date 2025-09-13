f9f256bd7e2227f8e6a211786cfdb5d1482d1aa8
Derek <derekgoodman@outlook.com>
2025-09-13 02:34:00 -0400

    Update poke.py

--- poke.py
+++ poke.py
@@ -1,124 +1,72 @@
-import discord, requests, random, asyncio, os
+import discord, random, os, subprocess
 from dotenv import load_dotenv
+from movieFunctions import (userInput, getActor, getRandomMovie, getMovieSearch, getMovieDetails, getFilmography, getActorID)
+from genreData import GENRES
+
 
 load_dotenv()
 API_KEY = os.getenv("MOVIE_API_KEY")
-DISCORD_TOKEN = os.getenv("POKE_DISCORD_TOKEN")
-
-GENRES = {
-    "action": 28,
-    "adventure": 12,
-    "animation": 16,
-    "comedy": 35,
-    "crime": 80,
-    "documentary": 99,
-    "drama": 18,
-    "family": 10751,
-    "fantasy": 14,
-    "horror": 27,
-    "romance": 10749,
-    "sci-fi": 878,
-    "thriller": 53,
-    "western": 37,
-    "war": 10752,
-    "mystery": 9648,
-    "history": 36,
-    "music": 10402
-}
+DISCORD_TOKEN = os.getenv("CINEBOT_DISCORD_TOKEN")
 
 class MyClient(discord.Client):
-    async def getRandomMovie(self, genreID):
-        url = "https://api.themoviedb.org/3/discover/movie?api_key=" + API_KEY + "&with_genres=" + str(genreID) + "&language=en-US"
-        response =  requests.get(url).json()
-        randomMovie = random.choice(response["results"])
-    
-    async def getMovieSearch(self,movieSearch, message):
-        url = "https://api.themoviedb.org/3/search/movie?api_key=" + API_KEY + "&query=" + movieSearch
-        response = requests.get(url).json()
-        if not response["results"]:
-            await message.channel.send("Sorry, I don't recognize this movie")
-        movie = response["results"][0]
-        return movie
-
-    async def userInput(self, prompt, message):
-        await message.channel.send(prompt)
-        try:
-            msg = await self.wait_for("message", timeout = 30)
-            return msg.content.lower()
-        except asyncio.TimeoutError:
-            await message.channel.send('Sorry Took To Long To Respond')
-
-    async def getMovieDetails(self, movie, message):
-         ageRating = await self.getAgeRating(movie)  
-         poster = movie["poster_path"]
-         await self.getPoster(poster, message)
-         await message.channel.send("🎬 **" + movie["title"] + "**\nRelease Date: " + movie["release_date"] + "\nAge Rating: " + ageRating + "\nOverview: " + movie["overview"])
-        
-    async def getActor(self, actorName, message):
-        url = "https://api.themoviedb.org/3/search/person?api_key=" + API_KEY + "&query=" + actorName
-        response = requests.get(url).json()
-        if not response["results"]:
-            await message.channel.send("Sorry, I don't recognize this actor")
-        knownFor = []
-        for n in response["results"]:
-            for m in n["known_for"]:
-                knownFor.append(m)
-
-        return knownFor
-    
-    async def getPoster(self, poster, message):
-        if poster:
-            img = "https://image.tmdb.org/t/p/w500" + poster
-            await message.channel.send(img)
-        else:
-            message.channel.send("Sorry no poster found")
-
-    async def getAgeRating(self, movie):
-        certification_url = f"https://api.themoviedb.org/3/movie/{movie['id']}/release_dates?api_key={API_KEY}"
-        certResponse = requests.get(certification_url).json()
-        ageRating = "NR"  
-        for c in certResponse["results"]:
-            if c["iso_3166_1"] == "US":
-                for r in c["release_dates"]:
-                    if r.get("certification"):
-                        ageRating = r["certification"]
-                        break
-                break
-        
-        return ageRating
-
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
+
+    async def startMessage(self, guild):
+        for channel in guild.text_channels:
+            if channel.permissions_for(guild.me).send_messages:
+                await channel.send("Hi everyone! Enter $info To Get Started!")
+                break
+    
     async def on_message(self, message):
+
         if message.author == self.user:
             return
-        if message.content.startswith('$poke'):
-            await message.channel.send('Hi, I am called PokeBot, I give movie recommendations based on what genre you like! Please Enter "$movie" To Start')
-        if message.content.startswith('$movie'):
-            genreName = await self.userInput("What Genre Of Movie Do You Want To Watch", message)
+        
+        if message.content.startswith('$info'):
+            await message.channel.send("Hi, I am called PokeBot, I give information and recommendations for movies!" 
+                                       + "__**\n Commands: **__" 
+                                       + "\n $movieRandom: Recommends a movie based on the genre you want to watch" 
+                                       + "\n $actorRandom: Recommends a movie based on an actor you want to watch"
+                                       + "\n $actorKnown: Tells you what movies the actor is known for"
+                                       + "\n $actorFilms: Tells you the entire filmography of an actor"
+                                       + "\n $search: Gets details on movies you want to search for"
+                                       + "\n $stop: Resets and stops all current actions of the bot")
+
+        if message.content.startswith('$movieRandom'):
+            genreName = await userInput(self, "What Genre Of Movie Do You Want To Watch", message)
             genreID = GENRES.get(genreName)
             if not genreID:
                 await message.channel.send('I dont recognize this genre, my apolgies')
                 return
-            randomMovie = await self.getRandomMovie(genreID)
-            await self.getMovieDetails(randomMovie, message)
+            randomMovie = await getRandomMovie(genreID)
+            await getMovieDetails(randomMovie, message)
            
         if message.content.startswith("$actorRandom"):
-            actorName = await self.userInput("What Is The Name Of The Actor You Want To Watch?", message)
-            knownFor = await self.getActor(actorName, message)
+            actorName = await userInput(self, "What Is The Name Of The Actor You Want To Watch?", message)
+            knownFor = await getActor(actorName, message)
             randomMovie =  random.choice(knownFor)
-            await self.getMovieDetails(randomMovie, message)
+            await getMovieDetails(randomMovie, message)
            
-        if message.content.startswith("$actorKnownfor"):
-            actorName = await self.userInput("What Is The Name Of The Actor You Want To Watch?", message)
-            knownFor = await self.getActor(actorName, message)
+        if message.content.startswith("$actorKnown"):
+            actorName = await userInput(self, "What Is The Name Of The Actor You Want To Watch?", message)
+            knownFor = await getActor(actorName, message)
             for i in knownFor:
-                await self.getMovieDetails(i, message)
+                await getMovieDetails(i, message)
+        if message.content.startswith("$actorFilms"):
+            actorName = await userInput(self, "What Is The Name Of The Actor You Want To Watch?", message)
+            actorID = await getActorID(actorName, message)
+            await getFilmography(actorID, message)
 
         if message.content.startswith("$search"):
-             movieSearch = await self.userInput("What Movie Do You Want To Search For?", message)
-             movie = await self.getMovieSearch (movieSearch, message)
-             await self.getMovieDetails(movie, message)
+             movieSearch = await userInput(self, "What Movie Do You Want To Search For?", message)
+             movie = await getMovieSearch (movieSearch, message)
+             await getMovieDetails(movie, message)
+        
+        if message.content.startswith("$stop"):
+            await self.close()
+            subprocess.run(["python", "poke.py"])
+
 
 intents = discord.Intents.default()
 intents.message_content = True
